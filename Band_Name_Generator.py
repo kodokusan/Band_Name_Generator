a52eac8f194b6d191e4cf7bb96cca3191aeb6c35
Kodoku <svandegrift@icloud.com>
2022-10-06 18:39:24 -0500

    Create Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+print("Welcome to the Band Name Generator")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pets name?\n")
+print(f"Your band name could be {city} {pet}")
